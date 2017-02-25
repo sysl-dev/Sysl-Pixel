baede15a81e69367cbe670c4928d9b82040dfee2
C.Hall <Systemlogoffsa@gmail.com>
2017-02-25 13:04:27 -0700

    Beta Release
    Beta release of lovepixels.

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+  t.title             = "lovePixel Library Test File"
+  t.author            = "Chris (DarkSysL/System Logoff/Darkdata)"
+  t.version           = "0.10.0"
+  t.identity          = "lovePixel-1"
+  t.build             = "1.0"
+end
